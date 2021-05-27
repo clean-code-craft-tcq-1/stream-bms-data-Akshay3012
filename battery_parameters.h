71a129f33806bda9eaaf250a2b1b58a2f648082f
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-05-27 19:52:37 +0530

    Update battery_parameters.h

--- battery_parameters.h
+++ battery_parameters.h
@@ -1,1 +1,13 @@
+#pragma once
+#include<map>
 
+class BMSParameters
+{
+
+public:
+
+    std::vector<int> getBatteryTempValuesInRange(int minTempVal, int maxTempVal);
+    std::vector<int> getBatterySOCValuesInRange(int minSOCVal, int maxSOCVal);
+    std::vector<int> generateRandomPrameters(int minVal, int maxVal);
+    bool sendParameterValuesToConsole(std::string parameterName, int minVal, int maxVal);
+};
