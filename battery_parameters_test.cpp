e9198b4b0f1d354e4f06dab191fa0f09646a72e5
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-05-28 21:42:38 +0530

    Update battery_parameters_test.cpp

--- battery_parameters_test.cpp
+++ battery_parameters_test.cpp
@@ -1,1 +1,20 @@
+#define CATCH_CONFIG_MAIN
+#include "test/catch.hpp"
+#include"battery_parameters.h"
 
+TEST_CASE(" Parameter values"){
+ 
+	int minTempVal = 0;   
+	int maxTempVal = 100;
+	int minSOCVal = 0;   
+	int maxSOCVal = 100;
+  std::string parameterName1 = "Temperature";
+  std::string parameterName2 = "SOC";
+  std::string wrongParameterName = "Charging Rate";
+  
+	BMSParameters obj_BMSParameters;
+  
+  REQUIRE(obj_BMSParameters.sendParameterValuesToConsole(parameterName1, minTempVal, maxTempVal) == true);
+  REQUIRE(obj_BMSParameters.sendParameterValuesToConsole(parameterName2, minSOCVal, maxSOCVal) == true);
+  REQUIRE(obj_BMSParameters.sendParameterValuesToConsole(wrongParameterName, minTempVal, maxTempVal) == false);	
+}
