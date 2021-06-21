70ba793af75481f2251aa61e7f8603de4b20c4ff
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-06-21 21:33:21 +0530

    Update Receiver_DataCalc.h

--- Bms_Receiver/Receiver_DataCalc.h
+++ Bms_Receiver/Receiver_DataCalc.h
@@ -13,11 +13,14 @@ static int rv_array[20];
 static char rv_data[500];
 
 /* ***************************** Prototypes ***************************** */
-float Calc_Maxi();
-float Calc_Mini(float current_value, float prev_mini);
-float Calc_Average(float Val_Arr[], int NoOfValues);
+int Calc_Maxi(int Val_Arr[]);
+int Calc_Mini(int Val_Arr[]);
+float Calc_Average(int Val_Arr[]);
 
 void readfromconsole();
 bool compareDataFromStream(int send_data[],int receive_data[]);
+bool compareMaxDataFromStream(int send_data[],int receive_data[]);
+bool compareMinDataFromStream(int send_data[],int receive_data[]);
+bool compareAverageDataFromStream(int send_data[],int receive_data[]);
 
 #endif
