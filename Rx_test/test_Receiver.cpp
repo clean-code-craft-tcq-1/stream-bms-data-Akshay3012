9b7cea2085ce8dfe9c0581c1cd99ad98bfd2b92b
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-06-21 20:03:45 +0530

    Update test_Receiver.cpp

--- Rx_test/test_Receiver.cpp
+++ Rx_test/test_Receiver.cpp
@@ -8,67 +8,34 @@
 
 //printstream();
 
-TEST_CASE("Test case to test the Maximum Value with lesser Value")
-{
-  float current_value = 10.1;
-  float prev_maxi = 20.2;
-  float max_value = 20.2;
-  REQUIRE(Calc_Maxi(current_value, prev_maxi) == max_value);
+TEST_CASE("Case for Postive test case :- Success of Data readFromConsole") {
+
+  REQUIRE(readfromConsole(Temperature, SOC) == Success);
+}
+/*Positive test case */
+TEST_CASE("Case for Positive test case :-Success of finding temperature array Minimum value") {
+	UserRequestOperation Operator= ToFindMinValue;
+  REQUIRE(FindMinandMaxValue(Temperature,Operator) == Success);
 }
 
-TEST_CASE("Test case to test the Maximum Value with greater Value")
-{
-  float current_value = 100.8;
-  float prev_maxi = 2;
-  float max_value = 100.8;
-  REQUIRE(Calc_Maxi(current_value, prev_maxi) == max_value);
+/*Positive test case */
+TEST_CASE("Case for Positive test case :-Success of finding SOC array Minimum value") {
+  UserRequestOperation Operator= ToFindMinValue;
+  REQUIRE(FindMinandMaxValue(SOC,Operator) == Success);
 }
 
-TEST_CASE("Test case to test the Maximum Value with Negative Value")
-{
-  float current_value = -1.8;
-  float prev_maxi = -29;
-  float max_value = -1.8;
-  REQUIRE(Calc_Maxi(current_value, prev_maxi) == max_value);
+/*Positive test case */
+TEST_CASE("Case for Positive test case:-Success of finding SOC array Maximum value value") {
+  UserRequestOperation Operator= ToFindMaxValue;
+  REQUIRE(FindMinandMaxValue(SOC,Operator)  == Success);
 }
 
-TEST_CASE("Test case to test the Minimum Value with lesser Value")
-{
-  float current_value = 9.8;
-  float prev_mini = 11.8;
-  float mini_value = 9.8;
-  REQUIRE(Calc_Mini(current_value, prev_mini) == mini_value);
+TEST_CASE("Case for evaluating success of Moving average of temperature") {
+
+  REQUIRE(FindMovingAverage(Temperature,lengthOfInputData) == Success);
 }
 
-TEST_CASE("Test case to test the Minimum Value with greater Value")
-{
-  float current_value = 187.5;
-  float prev_mini = 181.8;
-  float mini_value = 181.8;
-  REQUIRE(Calc_Mini(current_value, prev_mini) == mini_value);
-}
+TEST_CASE("Case for evaluating success of Moving average of SOC") {
 
-TEST_CASE("Test case to test the Minimum Value with negative Value")
-{
-  float current_value = -0.5;
-  float prev_mini = -1.8;
-  float mini_value = -1.8;
-  REQUIRE(Calc_Mini(current_value, prev_mini) == mini_value);
-}
-
-TEST_CASE("Test case to test the Average Value")
-{
-  float arr[] = {10.3,10.1};
-  float average = (10.3+10.1)/2;
-  float calc_aver = 0;
-  float epsilon = 0.001;
-  calc_aver = Calc_Average(arr, 2);
-  REQUIRE(abs(calc_aver - average) < epsilon);
-}
-
-TEST_CASE("Test case to test the empty array for average")
-{
-  float arr[] = {};
-  float average = 0;
-  REQUIRE(Calc_Average(arr, 0) == average);
+  REQUIRE(FindMovingAverage(SOC,lengthOfInputData) == Success);
 }
