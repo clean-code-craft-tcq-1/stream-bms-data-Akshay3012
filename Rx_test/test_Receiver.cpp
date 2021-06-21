01195fae4b3fb5012b86ed774a424ab9707d0cda
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-06-21 20:18:39 +0530

    Update test_Receiver.cpp

--- Rx_test/test_Receiver.cpp
+++ Rx_test/test_Receiver.cpp
@@ -4,38 +4,39 @@
 #include "../Bms_Receiver/Receiver_DataCalc.h"
 #include "stddef.h"
 #include "string.h"
+#include "stdbool.h"
 
 
 //printstream();
 
 TEST_CASE("Case for Postive test case :- Success of Data readFromConsole") {
 
-  REQUIRE(readfromconsole() == Success);
+  REQUIRE(readfromconsole() == true);
 }
 /*Positive test case */
 TEST_CASE("Case for Positive test case :-Success of finding temperature array Minimum value") {
 	UserRequestOperation Operator= ToFindMinValue;
-  REQUIRE(FindMinandMaxValue(Temperature,Operator) == Success);
+  REQUIRE(FindMinandMaxValue(Temperature,Operator) == true);
 }
 
 /*Positive test case */
 TEST_CASE("Case for Positive test case :-Success of finding SOC array Minimum value") {
   UserRequestOperation Operator= ToFindMinValue;
-  REQUIRE(FindMinandMaxValue(SOC,Operator) == Success);
+  REQUIRE(FindMinandMaxValue(SOC,Operator) == true);
 }
 
 /*Positive test case */
 TEST_CASE("Case for Positive test case:-Success of finding SOC array Maximum value value") {
   UserRequestOperation Operator= ToFindMaxValue;
-  REQUIRE(FindMinandMaxValue(SOC,Operator)  == Success);
+  REQUIRE(FindMinandMaxValue(SOC,Operator)  == true);
 }
 
 TEST_CASE("Case for evaluating success of Moving average of temperature") {
 
-  REQUIRE(FindMovingAverage(Temperature,lengthOfInputData) == Success);
+  REQUIRE(FindMovingAverage(Temperature,lengthOfInputData) == true);
 }
 
 TEST_CASE("Case for evaluating success of Moving average of SOC") {
 
-  REQUIRE(FindMovingAverage(SOC,lengthOfInputData) == Success);
+  REQUIRE(FindMovingAverage(SOC,lengthOfInputData) == true);
 }
