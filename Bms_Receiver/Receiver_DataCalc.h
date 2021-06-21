f3d760b777f0024ef817580e5fc90bfa5ccecc94
arjunsivakumar07 <77655050+arjunsivakumar07@users.noreply.github.com>
2021-06-21 19:16:54 +0530

    Update Receiver_DataCalc.h

--- Bms_Receiver/Receiver_DataCalc.h
+++ Bms_Receiver/Receiver_DataCalc.h
@@ -13,6 +13,6 @@
 float Calc_Maxi(float current_value, float prev_maxi);
 float Calc_Mini(float current_value, float prev_mini);
 float Calc_Average(float Val_Arr[], int NoOfValues);
-int printstream();
+//int printstream();
 
 #endif
